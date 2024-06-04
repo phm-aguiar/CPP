7fd9bd301b4c8ab65b8683b3884da868a8a6dd49
phenriq2 <phenriq2@student.42sp.org.br>
2024-06-04 11:12:35 -0300

    declaration of the Contats class with all its functions, constructors and destructors, following the canonical way of writing classes

--- CPP00/ex01/Contats.hpp
+++ CPP00/ex01/Contats.hpp
@@ -6,30 +6,38 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 17:11:10 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/06/01 21:16:03 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/06/03 18:08:49 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iomanip>
 #include <iostream>
 
 class Contats
 {
-	std::string nome;
-	std::string sobrenome;
-	std::string numCont;
-	std::string secret;
+	std::string _nome;
+	std::string _sobrenome;
+	std::string _nickname;
+	std::string _numCont;
+	std::string _secret;
 
   public:
 	Contats();
 	~Contats();
 	Contats(const Contats &copia);
 	Contats &operator=(const Contats &backup);
+	void printCont(size_t index);
 	void setNome(const std::string nome);
 	void setSobrenome(const std::string sobrenome);
-	void setNumCont(long numCont);
+	void setNumCont(std::string numCont);
 	void setSecret(const std::string secret);
+	void setNickname(const std::string nickname);
+	// void setIndex(int index);
+	std::string getNickname();
 	std::string getNome();
 	std::string getSobrenome();
 	std::string getNumCont();
 	std::string getSecret();
+	int getIndex();
+
 };
