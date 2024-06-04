8331c533a07c8ca5cd84a7ba8a302b8a829481c0
phenriq2 <phenriq2@student.42sp.org.br>
2024-06-04 11:23:29 -0300

    construction of the phone book class according to what is required in the subject

--- CPP00/ex01/Phonebook.hpp
+++ CPP00/ex01/Phonebook.hpp
@@ -6,13 +6,16 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 16:37:02 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/06/01 20:31:19 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/06/03 17:54:57 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Contats.hpp>
 #include <sstream>
 #include <limits>
+#include <cstdlib>
+
+
 # define RESET   "\033[0m"
 # define RED     "\033[31m"
 # define GREEN   "\033[32m"
@@ -27,4 +30,5 @@ class Phonebook
 	Phonebook& operator=(const Phonebook& backup);
 	void add(void);
 	void search(void);
+	bool isNumber(std::string numCont);
 };
